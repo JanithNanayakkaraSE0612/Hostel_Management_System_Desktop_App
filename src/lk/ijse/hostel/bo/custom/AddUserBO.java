240352ff23f79d5f802e674cc8aa7baa47bbfe52
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-08-19 17:57:26 +0530

    added

---
+++ src/lk/ijse/hostel/bo/custom/AddUserBO.java
@@ -0,0 +1,4 @@
+package lk.ijse.hostel.bo.custom;
+
+public interface AddUserBO extends lk.ijse.hostel.bo.custom.StudentBO {
+}
---
+++ src/lk/ijse/hostel/dao/custom/QueryDAO.java
@@ -0,0 +1,11 @@
+package lk.ijse.hostel.dao.custom;
+
+import lk.ijse.hostel.dao.SuperDAO;
+import lk.ijse.hostel.entity.CustomEntity;
+
+
+import java.util.ArrayList;
+
+public interface QueryDAO extends SuperDAO {
+    ArrayList<CustomEntity> getAllPendingPaymentStudent();
+}
